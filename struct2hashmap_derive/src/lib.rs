2d74935f34b8905ad063682e271d07f626110307
Sergey Chernomorets <chernomorets@superjob.ru>
2020-02-18 12:39:27 +0300

    fix: more readable error messages

--- struct2hashmap_derive/src/lib.rs
+++ struct2hashmap_derive/src/lib.rs
@@ -7,11 +7,16 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
+use quote::quote_spanned;
 use syn;
+use syn::spanned::Spanned;
 
 #[proc_macro_derive(ToHashMap)]
 pub fn to_hashmap(input: TokenStream) -> TokenStream {
-    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+    let ast: syn::DeriveInput = match syn::parse(input){
+        Ok(a) => a,
+        Err(e) => return e.to_compile_error().into(),
+    };
 
     let struct_name = &ast.ident;
     let fields = match ast.data {
@@ -19,12 +24,21 @@ pub fn to_hashmap(input: TokenStream) -> TokenStream {
             syn::Fields::Named(fields) => {
                 fields.named.clone()
             },
-            _ => panic!("You can only derive this on structs with named fields!"),
+            _ => return quote_spanned! {
+                datastruct.struct_token.span => compile_error!("You can only derive this on structs with named fields!");
+            }.into(),
         },
-        _ => panic!("You can only derive this on structs with named fields!"),
+        _ => return quote_spanned! {
+            ast.ident.span() => compile_error!("You can only derive this on structs with named fields!");
+        }.into(),
     };
     let inserts = fields.iter().map(|field| {
-        let ident = field.ident.as_ref().unwrap();
+        let ident = match field.ident.as_ref(){
+            Some(i) => i,
+            None => return quote_spanned! {
+                field.ty.span()=>compile_error!("You can only derive this on structs with named fields!");
+            }.into(),
+        };
         let key = ident.to_string();
         quote! {
             hm.insert(#key.to_string(), format!("{}", self.#ident))
@@ -32,7 +46,12 @@ pub fn to_hashmap(input: TokenStream) -> TokenStream {
     });
 
     let inserts_prefix = fields.iter().map(|field| {
-        let ident = field.ident.as_ref().unwrap();
+        let ident = match field.ident.as_ref(){
+            Some(i) => i,
+            None => return quote_spanned! {
+                field.ty.span()=>compile_error!("You can only derive this on structs with named fields!");
+            }.into(),
+        };
         let key = ident.to_string();
         quote! {
             hm.insert(format!("{}{}", prefix, #key.to_string()), format!("{}", self.#ident))
